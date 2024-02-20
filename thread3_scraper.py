505ab28cf9abf5a8449808603a660af7b584f6f3
allan <sequeira_36@hotmail.com>
2024-02-20 19:25:28 +0100

    thread stranka

--- thread3_scraper.py
+++ thread3_scraper.py
@@ -1,9 +1,12 @@
+import threading
 import urllib.request
 import time
+
 
 def download_site(url):
     with urllib.request.urlopen(url) as response:
         print(f"Stiahnutie {url}: {len(response.read())} bytov")
+
 
 sites = [
     "https://www.facebook.com/",
@@ -13,14 +16,19 @@ sites = [
     "https://www.facebook.com/",
     "https://docs.python.org/3/library/unittest.html#assert-methods",
     "https://www.mzv.sk/",
-    #"https://www.minv.sk/",
-    #"https://www.minv.sk/?NAKA"
+    # "https://www.minv.sk/",
+    # "https://www.minv.sk/?NAKA"
 
 ]
-
+threads = []
 start_time = time.time()
-for site in sites:
-    download_site(site)
+
+for url in sites:
+    thread = threading.Thread(target=download_site, args=(url,))
+    threads.append(thread)
+    thread.start()
+for thread in threads:
+    thread.join()
 
 duration = time.time() - start_time
 print(f"Stiahnutých {len(sites)} stránok za {duration} sekúnd")
